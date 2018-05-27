8b8289709815bb9a643243cdd23c034ccbdf964d
Bixing Xie <bx357@nyu.edu>
2018-05-27 16:43:15 -0400

    without string conversion

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -4,22 +4,40 @@ class Solution:
         :type x: int
         :rtype: bool
         """
-        #not optimized and used string, 53%
-        x = str(x)
-        if(len(x) == 0):
+        
+        #not converting to string, 50%
+        if x < 0:
             return False 
-        elif(len(x) == 1):
-            return True
         
-        start = 0 
-        end = len(x)-1
+        val = x 
+        rev = 0
         
-        while start < end:
-            if x[start] == x[end]:
-                start += 1
-                end -= 1
-            else: 
-                return False 
+        while x > 0: 
+            rev = rev * 10 + x % 10 
+            x //= 10
         
-        return True
+        return val == rev
+        
+        
+        #one liner 
+        # return str(x) == str(x)[::-1]
+    
+        #not optimized and used string, 53%
+#         x = str(x)
+#         if(len(x) == 0):
+#             return False 
+#         elif(len(x) == 1):
+#             return True
+        
+#         start = 0 
+#         end = len(x)-1
+        
+#         while start < end:
+#             if x[start] == x[end]:
+#                 start += 1
+#                 end -= 1
+#             else: 
+#                 return False 
+        
+#         return True
         
