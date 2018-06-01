408e71457a2ae2d1c7915225471e48a5667b151a
Bixing Xie <bx357@nyu.edu>
2018-06-01 19:54:25 -0400

    probelm 18

---
+++ two pointers/18. Four Sum.py
@@ -0,0 +1,52 @@
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        
+        #[-2, -1, 0, 0, 1, 2]
+        
+        res = []
+        
+        if len(nums) < 4: 
+            return res 
+
+        nums.sort()
+                
+        for index, a in enumerate(nums): 
+            
+            if index >= 1 and a == nums[index - 1]:
+                continue
+            
+            for  i in range (index+1, len(nums)):
+                b = nums[i]
+                
+                if i >= index + 2 and b == nums[i - 1]:
+                    continue
+
+                start = i + 1
+                end = len(nums) - 1
+                
+                while start < end: 
+                    c = nums[start]
+                    d = nums[end]
+
+                    if a + b + c + d == target:
+                        res.append([a, b, c, d])
+                        
+                        while start < end and nums[start] == nums[start + 1]:
+                            start += 1
+                        while start < end and nums[end] == nums[end - 1]:
+                            end -= 1
+                            
+                        start += 1
+                        end -= 1
+                        
+                    elif a + b + c + d > target: 
+                        end -= 1
+                    else:
+                        start += 1
+                        
+        return res
