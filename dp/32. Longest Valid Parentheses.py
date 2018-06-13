25ba7f67899c59f9fa9fc7113f702ccc42bb6756
Bixing Xie <bx357@nyu.edu>
2018-06-13 16:11:35 -0400

    add alternative solution

--- dp/32. Longest Valid Parentheses.py
+++ dp/32. Longest Valid Parentheses.py
@@ -1,29 +1,62 @@
 class Solution:
     def longestValidParentheses(self, s):
         """
-        time: O(n)
-        space: O(n)
-        :type s: str
-        :rtype: int
-        """
-        #()(())
-        #(())
-        #0, 1, 2, 3
-        if s == '': return 0 
-        dp = [0] * len(s)
+            time: O(n)
+            space: O(1)
+            faster solution
+            :type s: str
+            :rtype: int
+            """
         
-        for index, val in enumerate(s): 
-            if val == ')' and index > 0:
-                if s[index-1] == '(':
-                    if index > 1:
-                        dp[index] = dp[index-2] + 2
-                    else:
-                        dp[index] += 2
-                else:
-                    if index - dp[index-1] > 0 and s[index - dp[index-1] - 1] == '(':
-                        if index - dp[index-1] > 1: 
-                            dp[index] = dp[index-1] + dp[index - dp[index-1] - 2] + 2
-                        else:
-                            dp[index] = dp[index-1] + 2          
-        return max(dp)
-                
+        left = right = 0
+        res = 0
+        for index, val in enumerate(s):
+            if val == '(':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                res = max(res, 2 * left)
+            elif right > left:
+                left = right = 0
+    left = right = 0
+        for index, val in enumerate(s[::-1]):
+            if val == ')':
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                res = max(res, 2 * left)
+            elif right > left:
+                left = right = 0
+return res
+
+
+#     def longestValidParentheses(self, s):
+#         """
+#         time: O(n)
+#         space: O(n)
+#         :type s: str
+#         :rtype: int
+#         """
+#         #()(())
+#         #(())
+#         #0, 1, 2, 3
+#         if s == '': return 0
+#         dp = [0] * len(s)
+
+#         for index, val in enumerate(s):
+#             if val == ')' and index > 0:
+#                 if s[index-1] == '(':
+#                     if index > 1:
+#                         dp[index] = dp[index-2] + 2
+#                     else:
+#                         dp[index] += 2
+#                 else:
+#                     if index - dp[index-1] > 0 and s[index - dp[index-1] - 1] == '(':
+#                         if index - dp[index-1] > 1:
+#                             dp[index] = dp[index-1] + dp[index - dp[index-1] - 2] + 2
+#                         else:
+#                             dp[index] = dp[index-1] + 2
+#         return max(dp)
+
