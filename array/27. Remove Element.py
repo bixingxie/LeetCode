5cc19e1b5c6a58d5ffd79e6fb3680b7016234bc1
Bixing Xie <bx357@nyu.edu>
2018-06-22 23:29:13 -0400

    add array

---
+++ array/27. Remove Element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeElement(self, nums, val):
+        """
+        time: O(n)
+        space: O(1)
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+
+        i = 0 
+        for j in range(len(nums)):
+            if nums[j] != val: 
+                nums[i] = nums[j] 
+                i += 1
+        return i 
+        
